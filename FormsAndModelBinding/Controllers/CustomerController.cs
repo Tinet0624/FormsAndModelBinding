c9b48b7ae9970a99c778b3adce352a90d26e878a
Alexandra Haley <72041575+Tinet0624@users.noreply.github.com>
2021-07-22 19:06:41 -0700

    Add project files.

---
+++ FormsAndModelBinding/Controllers/CustomerController.cs
@@ -0,0 +1,54 @@
+﻿using FormsAndModelBinding.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FormsAndModelBinding.Controllers
+{
+    public class CustomerController : Controller
+    {
+        [HttpGet]
+        public IActionResult AddWithoutModelBinding()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddWithoutModelBinding(IFormCollection form)
+        {
+            Customer customer = new Customer()
+            {
+                FirstName = form["first-name"],
+                LastName = form["last-name"],
+                DateOfBirth = Convert.ToDateTime(form["dob"]),
+                Email = form["email"]
+            };
+
+            //Added to database
+
+            ViewData["addedWOBinding"] = $"{customer.FirstName} {customer.LastName} with the email {customer.Email} has been registered.";
+
+            return View();
+        }
+
+        // ModelBinding ----------------------------------------------------------------------
+        [HttpGet]
+        public IActionResult AddWithModelBinding()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddWithModelBinding(Customer c)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewData["Message"] = $"{c.FirstName} {c.LastName} with the email {c.Email} has been registered.";
+            }
+            return View();
+        }
+    }
+}
---
+++ FormsAndModelBinding/Models/Customer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FormsAndModelBinding.Models
+{
+    public class Customer
+    {
+        /// <summary>
+        /// First legal name of the customer.
+        /// </summary>
+        [Display(Name ="Full Name")]
+        [Required(ErrorMessage ="First name is required.")]
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// Last Legal name of the customer
+        /// </summary>
+        [Display(Name = "Last Name")]
+        [Required(ErrorMessage = "Last name is required.")]
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// Date customer was born
+        /// </summary>
+        [Display(Name = "Date of Birth")]
+        [DataType(DataType.Date)]
+        [Required]
+        public DateTime DateOfBirth { get; set; }
+
+        /// <summary>
+        /// Customer email
+        /// ex: customer@someplace.com
+        /// </summary>
+        [Display(Name = "Primary Email")]
+        [DataType(DataType.EmailAddress)]
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
